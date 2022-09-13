5a1a771ec9286e738dcd744b1fef3386faa62f2a
hee <jkgt0521@naver.com>
2022-09-13 22:51:32 +0900

    Feat #4 image request & response

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, send_file
 import os
 import torch
 from werkzeug.utils import secure_filename
@@ -7,10 +7,16 @@ app = Flask(__name__)
 
 model = torch.load('./ORGINAL_MODEL.pt', map_location='cpu')
 
-@app.route('/')
+@app.route('/api/v1/images/result', methods=['GET', 'POST'])
 def hello():
-    print(model)
-    return "모델 생성"
-
+    if request.method == 'GET':
+        print(model)
+        return "모델 생성"
+    if request.method == 'POST':
+        file = request.files['img']
+        fileName = file.filename
+        file.save(secure_filename(file.filename))
+        return send_file(fileName, mimetype='image/jpg')
+        
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
