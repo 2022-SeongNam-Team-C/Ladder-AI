48f055975db5208f02506e88786d2096d90bfbeb
hee <jkgt0521@naver.com>
2022-09-14 00:06:54 +0900

    Feat: #4 AI Module

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, send_file
-import os
-import torch
 from werkzeug.utils import secure_filename
+from .models import model_module
+
 
 app = Flask(__name__)
 
@@ -13,10 +13,11 @@ def hello():
         print(model)
         return "모델 생성"
     if request.method == 'POST':
-        file = request.files['img']
-        fileName = file.filename
-        file.save(secure_filename(file.filename))
-        return send_file(fileName, mimetype='image/jpg')
+        #file = request.files['img']
+        #fileName = file.filename
+        #file.save(secure_filename(file.filename))
+        # return send_file(fileName, mimetype='image/')
+        model_module.make_photo('https://news.mt.co.kr/mtview.php?no=2015040510223180585')
         
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
