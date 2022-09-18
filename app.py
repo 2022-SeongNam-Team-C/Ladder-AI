5f7c8597a065e1a7c9aece1fd0d15239c9253965
hee <jkgt0521@naver.com>
2022-09-18 22:00:24 +0900

    Fix: 확장자명 오류 수정

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from models.model_module import make_photo
+from api.model_module import make_photo
 
 app = Flask(__name__) 
 @app.route('/api/v1/images/result', methods=['POST'])
